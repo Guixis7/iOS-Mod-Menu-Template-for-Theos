3d1a46c505150bbe71fb9d9647856ce382b523a8
Guixis7 <gui.xis72210@gmail.com>
2025-01-22 20:59:38 +0000

    Pending changes exported from your codespace

---
+++ FFH4XJOBCheats/API/APIClient.h
@@ -0,0 +1,31 @@
+//
+//  APIClient.h
+//  APIClient
+//
+//  Created by Bao Nguyen on 01/09/2023.
+//  5.4.7 (1)
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface APIClient : NSObject
+- (void) paid:(void (^)(void))execute;
+- (void) setToken:(NSString*)token;
+- (void) setLanguage:(NSString*)language;
+- (void) hideUI:(bool)isHide;
+- (void) strictMode:(bool)_isStrictMode;
+- (void) silentMode:(bool)_isSilentMode;
+- (id)getPackageDataWithKey:(NSString*)key;
+- (NSString*) getKey;
+- (NSString*) getExpiryDate;
+- (NSString*) getUDID;
+- (NSString*) getDeviceModel;
+- (NSString*) getLoginIP;
+- (NSString*) getPackageName;
++ (instancetype) sharedAPIClient;
+@end
+
+NS_ASSUME_NONNULL_END
+
---
+++ FFH4XJOBCheats/FFH4XJOBCheats.h
@@ -0,0 +1,3 @@
+// Umbrella header for FFH4XJOBCheats.
+// Add import lines for each public header, like this: #import <FFH4XJOBCheats/XXXAwesomeClass.h>
+// Don’t forget to also add them to FFH4XJOBCheats_PUBLIC_HEADERS in your Makefile!
